210029d0ae4e5d0af7469f05ec954d528a261038
Christian Stuff <cstuff@ccstuf01.local>
2013-11-21 16:24:00 +0100

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+	$('.transition-switch input').on('change', function() {
+		$('.navbar-collapse').removeClass('old new').addClass($(this).val());
+	});
+	$('.navbar-toggle, .navbar-collapse li a').on('click', function() {
+		$('.navbar-collapse').toggleClass('in');
+	});
+	$('.panel-group .panel-title a').on('click', function(e) {
+		$(this).closest('.panel').find('.panel-collapse').toggleClass('in');
+		e.preventDefault();
+	});
+});
